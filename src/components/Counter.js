5078fed7d3f4fd962e50c27428e048af20003a43
Fin Mavis <fin.mavis@yahoo.com>
2019-01-30 22:25:28 +0700

    Add separate hooks counter

--- src/components/Counter.js
+++ src/components/Counter.js
@@ -1,31 +1,32 @@
-import React, { useState } from 'react';
+import React from 'react';
+import useCounter from '../hooks/counter';
 
 const Counter = props => {
-  const [counter, setCounter] = useState(0);
+  const state = useCounter(0);
 
   return (
     <div className='wrapper d-flex justify-content-center align-items-center'>
       <div className='counter border border-secondary rounded d-flex flex-column'>
         <div className='d-flex justify-content-center align-items-center counter-display text-secondary bg-light'>
-          <div className='display-1'>{counter}</div>
+          <div className='display-1'>{state.value}</div>
         </div>
         <div className='d-flex flex-row counter-panels'>
           <button
             type='button'
             className='btn btn-danger w-100'
-            onClick={() => setCounter(counter - 1)}>
+            onClick={state.decrement}>
             <i className='fas fa-minus' />
           </button>
           <button
             type='button'
             className='btn btn-info w-100'
-            onClick={() => setCounter(0)}>
+            onClick={state.reset}>
             <i className='fas fa-sync-alt' />
           </button>
           <button
             type='button'
             className='btn btn-success w-100'
-            onClick={() => setCounter(counter + 1)}>
+            onClick={state.increment}>
             <i className='fas fa-plus' />
           </button>
         </div>
