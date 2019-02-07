c1924a752e30d309774617cbee039854f4755096
Aris Rinardi <aris.rinardi@amarbank.co.id>
2019-02-07 11:36:17 +0700

    Update hooks dependencies and add hooks rules in eslint

--- src/components/Counter.js
+++ src/components/Counter.js
@@ -2,31 +2,28 @@ import React from 'react';
 import useCounter from '../hooks/counter';
 
 const Counter = props => {
-  const state = useCounter(0);
+  const { value, increment, decrement, reset } = useCounter(0);
 
   return (
     <div className='wrapper d-flex justify-content-center align-items-center'>
       <div className='counter border border-secondary rounded d-flex flex-column'>
         <div className='d-flex justify-content-center align-items-center counter-display text-secondary bg-light'>
-          <div className='display-1'>{state.value}</div>
+          <div className='display-1'>{value}</div>
         </div>
         <div className='d-flex flex-row counter-panels'>
           <button
             type='button'
             className='btn btn-danger w-100'
-            onClick={state.decrement}>
+            onClick={decrement}>
             <i className='fas fa-minus' />
           </button>
-          <button
-            type='button'
-            className='btn btn-info w-100'
-            onClick={state.reset}>
+          <button type='button' className='btn btn-info w-100' onClick={reset}>
             <i className='fas fa-sync-alt' />
           </button>
           <button
             type='button'
             className='btn btn-success w-100'
-            onClick={state.increment}>
+            onClick={increment}>
             <i className='fas fa-plus' />
           </button>
         </div>
